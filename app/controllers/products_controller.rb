d04a27b996a2a757148cdf45599ce8504cb70878
Ritika90 <erritika.1690@gmail.com>
2015-01-13 13:00:39 +0530

    fc

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,8 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-     @prod = Product.all
+     @products = User.all
+
   end
 
   # GET /products/1
